0e5ea762b0fc2e533f39043dc0978b7c484f8242
Gulbara Satybekova <gulbara.satybekova.on@gmail.com>
2022-01-30 21:22:15 +0600

    delete ul, correct css

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-function () {
-  document.getElementById("inputTodo").classList.toggle("show");
-}
+
+// const btn = document.querySelector(".btn");
+
+// btn.addEventListener("click", addToList);
+
+// function addToList( {
+//         userInput = input.value;
+// };
+
+const btn = document.querySelector(".btn");
+
+const input = document.querySelector("#inputTodo");
+
+const userInput = document.createElement("p");
+userInput.className = "usrer-input";
+userInput.style.border = "1px solid gray";
+userInput.style.borderRadius = "5px";
+userInput.style.padding = "20px";
+
+const todoOutput = document.querySelector("#todo-output");
+
+
+
+
+btn.addEventListener("click", addToList);
+
+function addToList(){
+    todoOutput.appendChild("userInput");
+     const userInput = input.value;
+
+
+};
